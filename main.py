9097e4ff2e45006d4740fe3df104cba8147429f0
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-10 13:44:32 +0530

    post category

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from database import engine, SessionLocal, Base
+from database import engine, SessionLocal, Base, get_db
 import models
+import schemas
 
 app = FastAPI()
 
@@ -10,5 +11,15 @@ Base.metadata.create_all(engine)
 @app.get("/")
 def home():
     return{
-        "message" : "DB Connection success"
+        "message" : "Home Page"
     }
+
+@app.post("/categories", response_model=schemas.CategoryOut)
+def create_category(category: schemas.Category, db : Session = Depends(get_db)):
+    category_dict = category.model_dump()
+    db_category = models.Category(**category_dict)
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+
+    return db_category
