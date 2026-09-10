9d94355f599e3e29c0dd55ede2338b729cba37a3
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-10 12:49:53 +0530

    date auto default

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float
 from sqlalchemy.orm import relationship
 from database import Base
+from datetime import date
 
 
 class Category(Base):
@@ -17,7 +18,7 @@ class Expenses(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     price = Column(Float)
-    date = Column(Date)
+    date = Column(Date, default=date.today)
 
     category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
     category = relationship("Category", back_populates="expenses")
