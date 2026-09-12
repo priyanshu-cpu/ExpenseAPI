b788065ed52859db499503105a5decd53f62be75
Priyanshu kumar <118802255+priyanshu-cpu@users.noreply.github.com>
2026-09-12 09:47:33 +0530

    Fix CORS origin URL by removing trailing slash

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontendforexpenseapi.netlify.app/"],
+    allow_origins=["https://frontendforexpenseapi.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
